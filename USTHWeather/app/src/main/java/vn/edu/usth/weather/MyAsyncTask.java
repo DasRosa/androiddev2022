89b8b28de582f0e7f94930fb2aced983924082d2
DasRosa <luhuyen108@gmail.com>
2021-11-03 17:10:58 +0700

    Practical Work 15

--- USTHWeather/app/src/main/java/vn/edu/usth/weather/MyAsyncTask.java
+++ USTHWeather/app/src/main/java/vn/edu/usth/weather/MyAsyncTask.java
@@ -2,11 +2,19 @@ package vn.edu.usth.weather;
 
 import android.app.Activity;
 import android.content.Context;
+import android.graphics.Bitmap;
+import android.graphics.BitmapFactory;
 import android.os.AsyncTask;
 import android.os.SystemClock;
+import android.util.Log;
+import android.widget.ImageView;
 import android.widget.Toast;
 
-public class MyAsyncTask extends AsyncTask<Void, Void, Void> {
+import java.io.InputStream;
+import java.net.HttpURLConnection;
+import java.net.URL;
+
+public class MyAsyncTask extends AsyncTask<String, Void, Bitmap> {
 
     private final Activity contextParent;
 
@@ -21,16 +29,30 @@ public class MyAsyncTask extends AsyncTask<Void, Void, Void> {
     }
 
     @Override
-    protected void onPostExecute(Void unused) {
-        super.onPostExecute(unused);
+    protected void onPostExecute(Bitmap param) {
+        super.onPostExecute(param);
         Toast.makeText(contextParent, "Finished", Toast.LENGTH_SHORT).show();
     }
 
     @Override
-    protected Void doInBackground(Void... voids) {
-        for (int i = 0; i <= 100; i++) {
-            SystemClock.sleep(100);
-        }
+    protected Bitmap doInBackground(String... param) {
+        URL url = new URL("http://ict.usth.edu.vn/wp-content/" +
+                "uploads/usth/usthlogo.png");
+        HttpURLConnection connection =
+                (HttpURLConnection) url.openConnection();
+        connection.setRequestMethod("GET");
+        connection.setDoInput(true);
+        connection.connect();
+
+        int response = connection.getResponseCode();
+        Log.i("USTHWeather", "The response is: " + response);
+        InputStream is = connection.getInputStream();
+
+        Bitmap bitmap = BitmapFactory.decodeStream(is);
+        ImageView logo = (ImageView) findViewById(R.id.logo);
+        logo.setImageBitmap(bitmap);
+        connection.disconnect();
+
         return null;
     }
 }
